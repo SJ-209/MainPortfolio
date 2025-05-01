f5e59bd78e8e43a3b18b6853768b4256696a5422
Quake <142627174+SJ-209@users.noreply.github.com>
2025-05-01 11:23:53 +0100

    projects section update

--- src/data.jsx
+++ src/data.jsx
@@ -18,12 +18,11 @@ export const projects = [
   },
   {
     title: "Photo Journal",
-    description: "",
+    description: "This Photo Journal project is a web application designed to display a collection of photographs with associated descriptions and locations. It was developed to explore and implement key front-end development principles, particularly the importance of component reusability and driving the user interface (UI) dynamically with data. The application likely features a visually appealing layout to present the journal entries.",
     image: "./assets/PJ.png",
     technologies: ["React", "JavaScript", "CSS", "HTML", "Render"],
     liveDemo: "https://photo-journal.onrender.com",
     github: "https://github.com/SJ-209/photo-journal",
-    impact: "Users commented on productivity increases of over 80% while using Fast Folders with Google Gemini",
   },
 ]
 
@@ -40,18 +39,6 @@ export const experiences = [
       "Proven ability to lead, adapt, and perform under pressure.",
     ],
   },
-  // {
-  //   company: "BA.",
-  //   role: "Section Commander/Sniper/SFC/Technical Engineer",
-  //   period: "2020 - Present",
-  //   achievements: [
-  //     "Served 17 years in the military, deploying globally to multiple operational theaters.",
-  //     "Held leadership roles such as Section Commander at ITC Catterick, training and mentoring new soldiers.",
-  //     "Operated as a Technical Engineer within a British Special Missions Unit, executing high-stakes operations in dynamic, high-pressure environments.",
-  //     "Experienced in remote operations, strategic planning, and rapid decision-making, delivering critical effects in fast-moving, high-intensity situations.",
-  //     "Proven ability to lead, adapt, and perform under pressure.",
-  //   ],
-  // },
 ]
 
 export const skills = [
